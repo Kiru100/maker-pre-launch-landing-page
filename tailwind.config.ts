97763ae36c60fb90de337a2d53302e32cfb36929
Kiru100 <79371033+Kiru100@users.noreply.github.com>
2025-01-02 22:38:06 +0800

    added colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,24 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+				"aqua-glow": "var(--aqua-glow)",
+				"deep-ocean": "var(--deep-ocean)",
+				"steel-mist": "var(--steel-mist)",
+				"pure-white": "var(--pure-white)",
+				"midnight-shadow": "var(--midnight-shadow)",
+				"crimson-alert": "var(--crimson-alert)",
+			},
+		},
+	},
+  	plugins: [],
 } satisfies Config;
